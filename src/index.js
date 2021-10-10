3b465342a8256e30c6af112b87f88e444377cbc9
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-10-10 21:43:55 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import 'modern-normalize/modern-normalize.css';
+// import './App.module.scss';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
